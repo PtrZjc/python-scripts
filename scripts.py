27a9cd114f36e9fac11c3c83c1f87269ae73b4ac
Zajac P <ptr.zjc@gmail.com>
2020-10-27 22:13:14 +0100

    finished script copying exif data'

--- scripts.py
+++ scripts.py
@@ -5,20 +5,19 @@ from os import walk
 import sys
 import subprocess
 
+
 @click.group()
 def cli():
     pass
 
-@cli.command('copy-exif-data')
+
+@cli.command('exif')
 @click.option('--source', '-s', help='input folder, default c:\\asource')
 @click.option('--target', '-t', help='target folder, default c:\\atarget')
 def copy_exif_data(source, target):
     """Copies exif tags from photos in source folder to target folder. Based only on names, extensions do not matter """
 
     exif_tool_path = os.path.join(sys.path[0], 'bin', 'exif', 'exiftool.exe')
-
-    # subprocess.run(exif_tool_path)
-    # usage: exiftool.exe -tagsFromFile" c:\asource\IMG_20201025_164020.jpg c:\atarget\IMG_20201025_164020.jpg
 
     if source == None:
         source = 'C:\\asource'
@@ -37,23 +36,27 @@ def copy_exif_data(source, target):
         target_photos.extend(filenames)
         break
 
-    source_photo_names = list(map(lambda x: x[:x.find('.')], source_photos))
-    target_photo_names = list(map(lambda x: x[:x.find('.')], target_photos))
-
     target_names = {}
+    source_names = {}
 
     for tphoto in target_photos:
         name = tphoto[:tphoto.find('.')]
         ext = tphoto[tphoto.find('.'):]
         target_names[name] = ext
 
-    click.echo(target_names)
-    click.echo(target_photo_names)
-    for sphoto in source_photo_names:
-        if sphoto in target_photo_names:
-            click.echo('TBD / file to change:')
-            click.echo(source + '\\' + sphoto + target_names[sphoto])
+    for sphoto in source_photos:
+        name = sphoto[:sphoto.find('.')]
+        ext = sphoto[sphoto.find('.'):]
+        source_names[name] = ext
 
+    click.echo(target_names)
+    click.echo(source_names)
+    for sphoto in source_names:
+        if sphoto in target_names:
+            file_to_change = target + '\\' + sphoto + target_names[sphoto]
+            source_file = source + '\\' + sphoto + source_names[sphoto]
+            click.echo('copying exif from %s to %s' %(source_file, file_to_change))
+            subprocess.run(exif_tool_path + ' -tagsFromFile '+ source_file + ' ' + file_to_change)
 
 if __name__ == '__main__':
     cli()
